2839b2f8bdbbb53109ef7bb74d91682334e9300c
TonyCross <starovoitov.anton@gmail.com>
2017-11-14 08:45:57 +0000

    added timezone

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,14 @@
 import requests
+import pytz
 from datetime import datetime
+from datetime import time
 
 
 def load_attempts():
-    response = requests.get(
-        'https://devman.org/api/challenges/solution_attempts/')
-    response_json = response.json()
-    pages = response_json['number_of_pages']
     current_page = 1
     all_attempts_list = []
-    for page in range(pages):
+    pages_quantity = 10
+    for page in range(pages_quantity):
         params = {'page': str(current_page)}
         response_json = requests.get(
             'https://devman.org/api/challenges/solution_attempts/',
@@ -20,17 +19,15 @@ def load_attempts():
 
 
 def get_midnighters(attempts_info):
+    midnight = time(hour=0, minute=0, microsecond=0)
+    morning = time(hour=9, minute=0, microsecond=0)
     solve_attempts = []
     records = attempts_info
     for record in records:
+        record_timezone = pytz.timezone(record['timezone'])
         record_time = record['timestamp']
-        utc_dt = datetime.utcfromtimestamp(record_time)
-        month = int(utc_dt.month)
-        day = int(utc_dt.day)
-        year = 2017
-        midnight = datetime(year=year, month=month, day=day, hour=0, minute=00)
-        morning = datetime(year=year, month=month, day=day, hour=9, minute=00)
-        if midnight < utc_dt < morning:
+        localized_record_time = record_timezone.localize(datetime.fromtimestamp(record_time))
+        if midnight < localized_record_time.time() < morning:
             solve_attempts.append(record)
     return solve_attempts
 
